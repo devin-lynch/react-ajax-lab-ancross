9a761c718fc2ddd5f4b08bdb8d67fd455b0a3491
devin-lynch <devin.l.lynch@gmail.com>
2022-09-29 17:49:42 -0500

    Search filter works

--- src/App.js
+++ src/App.js
@@ -4,25 +4,48 @@ import DisplayCards from './DisplayCards'
 
 export default function App() {
 
+  // API data
   let [data, setData] = useState({villagers: []})
+  // search input
+  let [search, setSearch] = useState('')
+  let [faves, setFaves] = useState([])
 
   useEffect(() => {
     fetch('http://acnhapi.com/v1/villagers/')
       .then(response => response.json())
-      .then((rdata) => {
-        rdata = Object.values(rdata)
-        setData({villagers: rdata})
-        console.log('Villager Data:', rdata)
+      .then((responseData) => {
+        // console.log(responseData)
+        responseData = Object.values(responseData)
+        setData({villagers: responseData})
+        console.log('Villager Data:', responseData)
       })
+      .catch(err => console.warn(err))
   }, [])
 
-  const villagerComponents = data.villagers.map((villager, i) => {
-    return <li>{villager.name['name-USen']}</li>
-  })
+  const getFilteredVillagers = () => {
+    const filteredVillagers = data.villagers.filter(villager => {
+      const name = villager.name['name-USen'].toLowerCase()
+      return name.includes(search.toLowerCase())
+    })
+    return filteredVillagers
+  }
+
+  getFilteredVillagers()
 
   return (
     <div className="App">
-      {villagerComponents}
+      <label htmlFor='search-input'>Search:</label>
+      <input
+        type='text'
+        placeholder='enter villager name...'
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+
+      <DisplayCards
+        villagers={getFilteredVillagers()}
+      />
+    
     </div>
   );
 }
--- src/DisplayCards.jsx
+++ src/DisplayCards.jsx
@@ -1,9 +1,19 @@
 
 
-export default function DisplayCards() {
+export default function DisplayCards(props) {
+    const villagers = props.villagers.map(villager => {
+        return (
+            <li key={`villager${villager.id}`} style={{ listStyleType: 'none' }}>
+                <img src={villager.image_uri} alt={villager.name['name-USen']} />
+
+                <p>{villager.name['name-USen']}</p>
+            </li>
+        )
+    } )
+
     return (
-        <div>
-            
-        </div>
+        <ul>
+            {villagers}
+        </ul>
     )
 }
