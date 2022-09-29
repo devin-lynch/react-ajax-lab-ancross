2086fea1a226766fbb1f27d60eab540a07b68819
devin-lynch <devin.l.lynch@gmail.com>
2022-09-29 15:54:18 -0500

    Add DisplayCards.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css';
+import DisplayCards from './DisplayCards'
 
 export default function App() {
 
@@ -10,15 +11,19 @@ export default function App() {
       .then(response => response.json())
       .then((rdata) => {
         rdata = Object.values(rdata)
+        setData({villagers: rdata})
         console.log('Villager Data:', rdata)
       })
   }, [])
 
+  const villagerComponents = data.villagers.map((villager, i) => {
+    return <li>{villager.name['name-USen']}</li>
+  })
+
   return (
     <div className="App">
-
+      {villagerComponents}
     </div>
   );
 }
-
 
---
+++ src/DisplayCards.jsx
@@ -0,0 +1,9 @@
+
+
+export default function DisplayCards() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
